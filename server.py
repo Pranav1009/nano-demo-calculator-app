51b0e3d8db16a82b3cbe34566b24d5291cfcb067
M Pranav <pranavmahesh2k2@gmail.com>
2023-08-15 22:15:43 +0530

    Endpoint written

--- server.py
+++ server.py
@@ -1,19 +1,28 @@
-from flask import Flask
+from flask import Flask,request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Hello world!", 200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.json
+    first_number = data['first']
+    second_number = data['second']
+    result = first_number + second_number
+    response = {'result': result}
+    return jsonify(response), 200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
-
+    data = request.json
+    first_number = data['first']
+    second_number = data['second']
+    result = first_number - second_number
+    response = {'result': result}
+    return jsonify(response), 200
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8000,host='0.0.0.0')
