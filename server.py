ac11acc329490984114e0bdd93ab34d05e87413f
M Pranav <pranavmahesh2k2@gmail.com>
2023-08-15 22:18:18 +0530

    Changed port to 8080

--- server.py
+++ server.py
@@ -25,4 +25,4 @@ def subtract():
     response = {'result': result}
     return jsonify(response), 200
 if __name__ == '__main__':
-    app.run(port=8000,host='0.0.0.0')
+    app.run(port=8080,host='0.0.0.0')
